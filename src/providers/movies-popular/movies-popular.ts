28695294dda0b1ed62396ddb03ae15a4f8b65538
SaintWise99 <preatorianwise999@gmail.com>
2023-02-09 19:58:06 -0300

    add url providers

--- src/providers/movies-popular/movies-popular.ts
+++ src/providers/movies-popular/movies-popular.ts
@@ -9,9 +9,17 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class MoviesPopularProvider {
-
+   url = 'https://api.themoviedb.org/3/movie/popular?api_key=34738023d27013e6d1b995443764da44';
   constructor(public http: HttpClient) {
     console.log('Hello MoviesPopularProvider Provider');
   }
-
+  getMoviesPopular(){
+    return new Promise(resolve=>{
+      this.http.get(this.url).subscribe(data=>{
+          resolve(data);
+      },error=>{
+        console.log(error);
+      });
+    });
+  }
 }
--- src/providers/movies-rates/movies-rates.ts
+++ src/providers/movies-rates/movies-rates.ts
@@ -9,9 +9,17 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class MoviesRatesProvider {
-
+ url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=34738023d27013e6d1b995443764da44';
   constructor(public http: HttpClient) {
     console.log('Hello MoviesRatesProvider Provider');
   }
-
+  getMoviesRates(){
+    return new Promise(resolve=>{
+      this.http.get(this.url).subscribe(data=>{
+          resolve(data);
+      },error=>{
+        console.log(error);
+      });
+    });
+  }
 }
